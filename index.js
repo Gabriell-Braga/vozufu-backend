2a5e3d65553fdee0f71620ce39292185e80b2a81
Gabriel Braga <gabribragandrade@gmail.com>
2025-03-18 22:45:57 -0300

    Keys Changes

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const admin = require("firebase-admin");
 require("dotenv").config();
 
 // Inicializa o Firebase
-const serviceAccount = require("./firebase-key.json"); // Arquivo da chave privada
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL
+  };
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
